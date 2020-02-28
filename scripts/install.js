424926de8f2209ed184ef082a1a888b117c1ba41
StephenS <Stephen.shorrock@gmail.com>
2020-02-28 15:45:53 +0000

    install the passport.js config

--- scripts/install.js
+++ scripts/install.js
@@ -3,33 +3,28 @@ var fs = require('fs-extra');
 var appDir = process.env.PWD;
 
 //Config already exist so we don't override
-if (!fs.existsSync(appDir + "/../../config/passport.js"))
-{
+if (!fs.existsSync(appDir + "/../../config/passport.js")) {
 	//Copy base acl config
-	fs.copy(appDir + "/templates/passport.js", appDir + "/../../config/passport.js", function (err)
-	{
-		if (err)
-		{
+	fs.copy(appDir + "/templates/passport.js", appDir + "/../../config/passport.js", function (err) {
+		if (err) {
 			console.log(err);
-		}
-		else
-		{
+		} else {
 			console.log("done write passport.js base config");
 		}
 	});
 }
-if (!fs.existsSync(appDir + "/../../views/auth"))
-{
-	//Copy base acl config
-	fs.copy(appDir + "/templates/auth", appDir + "/../../views/auth", function (err)
-	{
-		if (err)
-		{
-			console.log(err);
-		}
-		else
-		{
-			console.log("done write auth base views");
-		}
-	});
-}
+// if (!fs.existsSync(appDir + "/../../views/auth"))
+// {
+// 	//Copy base acl config
+// 	fs.copy(appDir + "/templates/auth", appDir + "/../../views/auth", function (err)
+// 	{
+// 		if (err)
+// 		{
+// 			console.log(err);
+// 		}
+// 		else
+// 		{
+// 			console.log("done write auth base views");
+// 		}
+// 	});
+// }
