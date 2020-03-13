f6452c0bd09dc3b744cc256eb6d617142714c84b
Stephen Shorrock <sms67@cam.ac.uk>
2020-03-13 12:14:48 +0000

    moved config into index:defaults and index:routes

--- index.js
+++ index.js
@@ -14,41 +14,13 @@ module.exports = function sailsHookPassportCued(sails) {
   //   configure: require('./configure')(sails),
   //   initialize: require('./initialize')(sails),
 
-  //   routes: {
-
-  //     // These intercept our requests:
-  //     before: {
-
-  //       'post /logout': 'AuthController.logout',
-  //       'get /login': 'AuthController.login',
-
-  //       'post /auth/local': 'AuthController.callback',
-  //       'post /auth/local/:action': 'AuthController.callback',
-
-  //       'post /auth/:provider': 'AuthController.callback',
-  //       'post /auth/:provider/:action': 'AuthController.callback',
-
-  //       'get /auth/:provider': 'AuthController.provider',
-  //       'get /auth/:provider/callback': 'AuthController.callback',
-  //       'get /auth/:provider/:action': 'AuthController.callback',
-  //     },
-  //     after: {
-  //       'GET /*': function (req, res, next) {
-  //         //sails.log('got a url');
-  //         //return next();
-  //         return res.send('not found');
-  //       }
-  //     }
-  //   }
-  // };
-
 
   // Override http config here before hook initialisation
   // We migh not need mvcsloader - and it might be tied to the old version of sails
   var hookLoader = require('sails-util-mvcsloader')(sails);
   hookLoader.configure({
     policies: __dirname + '/api/policies', // Path to your hook's policies
-    config: __dirname + '/config' // Path to your hook's config
+  //  config: __dirname + '/config' // Path to your hook's config
   });
   //sails.config.paths.models = [sails.config.paths.models, __dirname + '/api/models'];
   //sails.log('test');
@@ -57,7 +29,126 @@ module.exports = function sailsHookPassportCued(sails) {
 
   return {
     defaults: {
-      passport: {}
+      passport: {},
+      policies: {
+        'AuthController': {
+          'test': ['sessionAuth'],
+          '*': ['passport']
+        },
+        '*': ['passport', 'sessionAuth'] // insists all requests generate a passport session
+      },
+      http: {
+        middleware: {
+          passportCuedInit: require('passport').initialize(), // add after session
+          passportCuedSession: require('passport').session(), // add after passportCuedInit
+        }
+      },
+      passport: {
+        // a new user has been created - where do we direct them (we may not want auto enrol)
+        userVerifyRedirect: '/member/confirmacc',
+        // If this email domain exists remove from the username
+        removeEmailDomain: '<remove @.domain address>',
+        // Our index page
+        siteIndex: '/index',
+        hostname: 'http://localhost:1337',
+        loginView: '../node_modules/sails-hook-passport-cued/templates/auth/login',
+        loginLocal: '../node_modules/sails-hook-passport-cued/templates/auth/local', // where our login page lives
+        redirect: {
+          login: '/', //Login successful
+          logout: '/' //Logout successful
+        },
+        onUserCreated: function (user, providerInfos) {
+          //Send email for example
+        },
+        onUserLogged: function (session, user) {
+          //Set user infos in session for example
+        },
+        strategies: {
+      
+          local: {
+            status: 'inactive',
+            name: 'Local',
+            provider: 'local', // required
+            strategy: require('passport-local').Strategy
+          },
+          /*
+              twitter : {
+                name     : 'Twitter',
+                protocol : 'oauth',
+                strategy : require('passport-twitter').Strategy,
+                options  : {
+                  consumerKey    : 'your-consumer-key',
+                  consumerSecret : 'your-consumer-secret'
+                }
+              },
+      
+              facebook : {
+                name     : 'Facebook',
+                protocol : 'oauth2',
+                strategy : require('passport-facebook').Strategy,
+                options  : {
+                  clientID     : 'your-client-id',
+                  clientSecret : 'your-client-secret',
+                  scope        : ['email'] // email is necessary for login behavior
+        }
+      },
+      */
+          google: {
+            status: 'inactive',
+            name: 'Google',
+            protocol: 'oauth2',
+            strategy: require('passport-google-oauth').OAuth2Strategy,
+            options: {
+              clientID: '<Your client ID>',
+              clientSecret: '<Your client secret>',
+              scope: 'email',
+              hostedDomain: 'cam.ac.uk'
+            }
+          },
+          raven: {
+            status: 'inactive',
+            name: 'UcamWebauth',
+            protocol: 'WAAWLS', // NB protocol must be alpha numeric (not WAA->WLS) to store in passport
+            strategy: require('passport-raven').Strategy,
+            options: {
+              audience: 'http://<your host>:<your port>',
+              desc: ',Your application name here>',
+              msg: 'we need to check you are a current student'
+              // use demonstration raven server in development
+              //debug: process.env.NODE_ENV !== 'production',
+              //debug: false
+            },
+      
+            func: function (crsid, response, cb) {
+              console.dir(response);
+              cb(null, {
+                crsid: crsid,
+                isCurrent: response.isCurrent
+              });
+            }
+          }
+        }
+      }
+    },
+
+    routes: {
+      // These intercept our requests:
+      before: {
+        'post /logout': 'AuthController.logout',
+        'get /login': 'AuthController.login',
+      
+        'post /auth/local': 'AuthController.callback',
+        'post /auth/local/:action': 'AuthController.callback',
+      
+        'post /auth/:provider': 'AuthController.callback',
+        'post /auth/:provider/:action': 'AuthController.callback',
+      
+        'get /auth/:provider': 'AuthController.provider',
+        'get /auth/:provider/callback': 'AuthController.callback',
+        'get /auth/:provider/:action': 'AuthController.callback',
+      
+        'get /test': 'AuthController.test'
+      }
     },
 
     initialize: function (cb) {
